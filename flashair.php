d800187c72f07b73ed4824696adfa70b7da2ad5a
Tomas Gal <tomas.gal@upcmail.sk>
2015-07-30 20:06:14 +0200

    EoD Final Update

--- flashair.php
+++ flashair.php
@@ -4,13 +4,13 @@ function ping($host)
   exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($host)), $res, $rval);
   return $rval === 0;
 }
-$host = '192.168.1.5';
+$host = '192.168.1.99';
 $up = ping($host);
 $page = $_SERVER['PHP_SELF'];
 $sec = "10";
 
 echo '<img src="'.($up ? 'on' : 'off').'.jpg" alt="'.($up ? 'UP' : 'NO').'" />';
-echo " WD Cloud";
+echo " FlashAir";
 ?>
 
 <html><head>
